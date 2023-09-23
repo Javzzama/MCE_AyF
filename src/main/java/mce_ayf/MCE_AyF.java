5d2013d0b1ed3f048e626651c9e25875b250fd66
velaz <velaz@192.168.1.83>
2023-09-23 15:11:34 -0500

    Carga inicial

---
+++ src/main/java/mce_ayf/MCE_AyF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package mce_ayf;
+
+/**
+ *
+ * @author velaz
+ */
+public class MCE_AyF {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
